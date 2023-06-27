0bf48bd2b734c3e9af170db224a5d7c358d56161
DKKs <eye.q.21@gmail.com>
2023-06-28 05:44:59 +0700

    [Add] Swagger

--- backend/src/blog/dto/create-blog.dto.ts
+++ backend/src/blog/dto/create-blog.dto.ts
@@ -1,12 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 export class CreateBlogDto {
+  @ApiProperty()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   content: string;
 
+  @ApiProperty({
+    required: false,
+  })
   seo_title?: string;
+
+  @ApiProperty({
+    required: false,
+  })
   seo_description?: string;
+
+  @ApiProperty({
+    required: false,
+  })
   seo_keyword?: string;
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('CMS api')
+    .setDescription('CMS api for content')
+    .setVersion('1.0')
+    .addTag('content')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(4000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
